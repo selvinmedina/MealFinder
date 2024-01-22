8956cb248b99c2bb47dd03fbd0f32977335e30a7
Selvin Medina <selvinmedina0016@gmail.com>
2024-01-21 18:15:37 -0600

    Configuring routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
+import { DetailsComponent } from './pages/details/details.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent
+  },
+  {
+    path: 'details',
+    // path: 'details/:id',
+    component: DetailsComponent
+  }
+
+];
